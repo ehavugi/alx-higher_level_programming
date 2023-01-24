9c63938df8f5fa64c808e3ae94ab3fb850e87cdb
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-23 19:01:24 -0800

    Added exception handling

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    clean_list = []
+    for i in range(list_length):
+        result = 0
+        try:
+            result = my_list_1[i]/my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("Division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            clean_list.append(result)
+    return clean_list
