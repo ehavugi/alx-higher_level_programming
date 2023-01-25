04f553ec98a4508dad8d410a9cec8908a7fd9eb6
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-24 19:32:22 -0800

    added  a value set method

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+"""
+Square implementation v4
+added set method to set size to square v3
+"""
+
+
+class Square:
+    """
+    improved Square implementation v3
+    new features on top of v1
+        1. default size set to 0
+        2. type and value checking added to v1
+    Features on top of v2
+        + get value of area of square
+    """
+    def __init__(self, size=0):
+        """
+        Initialize the class by setting its attributes
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Returns the area of current square
+        """
+        return (self.__size)**2
+
+    def size(self, value):
+        """
+        Set value of size to value
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
