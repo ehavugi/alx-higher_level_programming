d05ae14c2f2d66b68c02690c1beb537a29cdf5fd
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-28 21:47:15 -0800

    initial commit -m

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+add_integer module  v0.0
+includes
+    1.doctests
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    >>> add_integer(1,1)
+    2
+    >>> add_integer(-8)
+    90
+    """
+    if not (type(a) in [int, float]):
+        raise (TypeError("a must be an integer"))
+    if not (type(b) in [int, float]):
+        raise (TypeError("b must be an integer"))
+
+    return int(int(a) + int(b))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
