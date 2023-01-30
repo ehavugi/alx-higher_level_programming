04ef9ea8a5a5bb8f19587c2db2cba2bfa9cbbf3c
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-29 22:00:59 -0800

    started working on more python classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+implements an empty class Rectangle
+
+"""
+
+
+class Rectangle:
+    """
+    An empty classs
+
+    """
+    def __init__(self):
+        """
+        class initialization
+        """
+        pass
