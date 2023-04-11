6baebc28434bca24703fbcb955828b26fe618bf2
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-11 15:52:53 +0000

    fixed 8-square to output missing size when value <0 or none

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (Number.isInteger(Number.parseInt(argv[2]))) {
+if (Number.parseInt(argv[2]) > 0) {
   for (let i = 0; i < Number.parseInt(argv[2]); i++) {
     console.log(''.padStart(Number.parseInt(argv[2]), 'x'));
   }
