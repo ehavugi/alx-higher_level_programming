e0b7e6ad060569792075bc15ccc1f642f91c79d4
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-13 05:07:45 +0000

    fixed square custom printer to use more than 1 char

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,8 +4,8 @@ const Square0 = require('./5-square');
 class Square extends Square0 {
   charPrint (c) {
     if (c !== undefined) {
-      for (let i = 1; i <= this.width; i++) {
-        console.log(''.padStart(this.height, String(c)));
+      for (let i = 1; i <= this.height; i++) {
+        console.log(String(c).repeat(this.width));
       }
     } else {
       this.print();
