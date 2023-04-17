955135ca33d27e954018f5422b3eba6a6dd85510
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-16 18:44:02 -0700

    use sqlalchemy directly

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,10 +2,10 @@
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+import sqlalchemy
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+    engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
