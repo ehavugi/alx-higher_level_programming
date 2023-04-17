6b4929b56a13ad6c478f0f2eef845497611e32b3
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-16 20:51:51 -0700

    added mandatory tasks

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Adds State object "Louisiana to db hbtn_0e_6_usa
+""" delete State object with state.name containing a
 """
 import sys
 from model_state import Base, State
@@ -10,13 +10,16 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
 
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
+    states_to_delete = session.query(State).filter(State.name.like('%a%'))
+    for state in states_to_delete:
+        session.delete(state)
     session.commit()
+
     session.close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" Adds State object "Louisiana to db hbtn_0e_6_usa
+""" update State object with state.id 2 to New Mexico
 """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
 
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
-    session.commit()
-    session.close()
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+    for city, state, in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
