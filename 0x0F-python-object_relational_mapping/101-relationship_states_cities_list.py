524fb0730aa2c0a27ca3c7b34cbc5b7e76d3c719
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-21 21:11:16 -0700

    fixed 101-relationship_states_city_list to use .all() and object methods

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -2,30 +2,21 @@
 """ update State object with state.id 2 to New Mexico
 """
 import sys
-from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
-from model_city import City
-
+from relationship_city import City, Base
+from relationship_state import State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
 
-    Session = sessionmaker()
-    Session.configure(bind=engine)
     session = Session()
-
-    states = session.query(State, City).join(State).order_by(
-             State.id).order_by(City.id).all()
-    old = ""
-    for state, city in states:
-        if state.name != old:
-            print("{}: {}".format(state.id, state.name))
-            print("    {}: {}".format(city.id, city.name))
-            old = state.name
-        else:
-            old = state.name
-            print("    {}: {}".format(city.id, city.name))
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print('{}:'.format(state.name))
+        for city in sorted(state.cities, key=lambda x: x.id):
+            print('\t{}: {}'.format(city.id, city.name))
