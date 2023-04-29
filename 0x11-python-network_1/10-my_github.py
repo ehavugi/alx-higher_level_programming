cf82ea2de597f83b0049917163f40092e4683b4e
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-29 01:10:35 -0700

    made sure the modules are not executed on import

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,7 +2,7 @@
 """ Read user ID given authentication """
 import sys
 import requests
-
-r = requests.get('https://api.github.com/user',
-                 auth=(sys.argv[1], sys.argv[2]))
-print(r.json().get("id"))
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    print(r.json().get("id"))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,8 +2,8 @@
 """ get 10 latest commits for a given user and repo"""
 import requests
 import sys
-
-commits = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
-          sys.argv[1], sys.argv[2]))
-for i in commits.json()[:10]:
-    print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+if __name__ == "__main__":
+    commits = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+              sys.argv[1], sys.argv[2]))
+    for i in commits.json()[:10]:
+        print("{}: {}".format(i['sha'], i['commit']['author']['name']))
