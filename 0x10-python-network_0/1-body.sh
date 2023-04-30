d59e19ce8d863b85849eb61e6ce0640608d1488f
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-30 16:00:40 -0700

    changed status to status_ to avoid use of special variables

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # use curl to get the body size
-status=$(curl -L -s --head "$1")
-body_=$(curl -L -s  "$1")
-check="200"
-if [[ "$status" == *"$check"* ]]; then
+status_=$(curl -s -L --head "$1")
+body_=$(curl -s -L  "$1")
+check="200 OK"
+if [[ "$status_" == *"$check"* ]]; then
 	 echo -n "$body_"
 fi
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # use curl to get the body size
-status=$(curl -L -s --head  -X DELETE "$1")
-body_=$(curl -L -s -X DELETE  "$1")
-check="200"
-if [[ "$status" == *"$check"* ]]; then
-	 echo -n "$body_"
-fi
+curl -sX DELETE  "$1"
