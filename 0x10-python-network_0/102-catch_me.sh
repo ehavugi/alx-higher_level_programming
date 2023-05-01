f206bd5c7c810d5985c2e999d2de6fc6faf04fa3
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-30 22:49:27 -0700

    catch you! the task revealed itself?

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get him, get the server to output a given tex CUrrently the redirects do continue to use PUT, they change to GET
-curl  -v -X PUT  -d "user_id"="98" 0.0.0.0:5000/catch_me
+curl  -X PUT  -d "user_id"="98" -H "Origin: School" 0.0.0.0:5000/catch_me_3
