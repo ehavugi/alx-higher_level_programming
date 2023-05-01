aed3afb5de5a60ffc5e4e221e251dbb1a43b4de4
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-30 20:28:45 -0700

    fixed number of lines

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # use curl to get http code  Author: Emmanuel HAVUGIMANA Date : 2023  For : ALX
-curl --header "Content-Type: application/json" -X POST --data-raw "$(cat "$2")"  "$1"
+curl --header "Content-Type: application/json" "$1" -X POST --data-raw "$(cat "$2")"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Get him, get the server to output a given text
-# CUrrently the redirects do continue to use PUT, they change to GET
+# Get him, get the server to output a given tex CUrrently the redirects do continue to use PUT, they change to GET
 curl  -v -X PUT  -d "user_id"="98" 0.0.0.0:5000/catch_me
-curl -v -X PUT -d "user_id"="98" 0.0.0.0:5000/catch_me_2
-curl -v -H "X-School-User-Id: 98" -X PUT -d "user_id"="98" 0.0.0.0:5000/catch_me_3
-echo 
