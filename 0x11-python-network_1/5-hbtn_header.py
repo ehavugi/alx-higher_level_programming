51d2e773e8d775837b0ad2483a0ffa963969fd16
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-30 21:07:37 -0700

    added status and header via requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Fetch data and retrieve X-Request-Id header"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with requests.get(url) as response:
+        print(response.headers.get("X-Request-Id", ""))
