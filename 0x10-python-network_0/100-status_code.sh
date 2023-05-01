4e32d7e2cb03578c7b6e813ff4871bd2733db664
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-30 20:08:17 -0700

    fixes #1: ?:

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# use curl to get http code 
-# Author: Emmanuel HAVUGIMANA
-# Date : 2023
-# For : ALX
+# use curl to get http code  Author: Emmanuel HAVUGIMANA Date : 2023 or : ALX
 curl -s -o /dev/null -w "%{http_code}" "$1"
