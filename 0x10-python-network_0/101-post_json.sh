c120be24eac7f0f1a2432972a91f2bade4514eac
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-04-30 20:10:17 -0700

    fixed number of lines to be 3

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# use curl to get http code 
-# Author: Emmanuel HAVUGIMANA
-# Date : 2023
-# For : ALX
+# use curl to get http code  Author: Emmanuel HAVUGIMANA Date : 2023  For : ALX
 curl --header "Content-Type: application/json" -X POST --data-raw "$(cat "$2")"  "$1"
